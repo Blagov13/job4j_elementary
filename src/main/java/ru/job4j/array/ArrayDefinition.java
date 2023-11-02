c7b3c603e1fff7376a1c71ab0b81b3f010a067a7
aleksandrblagov <blagov13rus@gmail.com>
2023-11-02 17:09:05 +0300

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,6 +5,15 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "Alex Blagov";
+        System.out.println(names[0]);
+        names[1] = "Daria Blagova";
+        System.out.println(names[1]);
+        names[2] = "Barni Blagov";
+        System.out.println(names[2]);
+        names[3] = "Komp Kompovich";
+        System.out.println(names[3]);
 
         System.out.println("Размер массива равен: " + ages.length);
         System.out.println("Размер массива равен: " + surnames.length);
